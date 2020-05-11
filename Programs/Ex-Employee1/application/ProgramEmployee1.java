4018ba76f5e0e2bc778859c907e06378f60376ee
Kaiquered <kaiquered@hotmail.com>
2020-05-11 12:29:31 -0300

    correção de erros

--- Programs/Ex-Employee1/application/ProgramEmployee1.java
+++ Programs/Ex-Employee1/application/ProgramEmployee1.java
@@ -29,7 +29,7 @@ public class ProgramEmployee1 {
 		
 		System.out.printf("Employee: %s, $ %.2f\n", employee.name , employee.netSalary());
 
-		System.out.printf("Which percentage to incinputase salary?");
+		System.out.printf("Which percentage to increase salary? ");
 		double percentage = input.nextDouble();
 		
 		System.out.printf("Updated data: %s, $ %.2f", employee.name ,employee.increaseSalary(percentage));
