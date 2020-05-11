b2e7bf3d2ef950a0e40a0222542fc8711aa3571f
Kaiquered <kaiquered@hotmail.com>
2020-05-11 11:58:02 -0300

    inserindo exemplo Employee

---
+++ Programs/Ex-Employee/application/ProgramEmployee1.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Employee1;
+
+/*Programa que calcula o salário líquido de um funcionário e aumenta seu salário baseado
+na quantia bruta.
+*/
+
+public class ProgramEmployee1 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+
+		Scanner input = new Scanner(System.in);
+
+		Employee1 employee = new Employee1();
+
+		System.out.printf("Name: ");
+		employee.name = input.nextLine();
+		
+		System.out.printf("Gross Salary: ");
+		employee.grossSalary = input.nextDouble();
+		
+		System.out.printf("Tax: ");
+		employee.tax = input.nextDouble();
+		
+		System.out.printf("Employee: %s, $ %.2f\n",employee.name,employee.netSalary());
+
+		System.out.printf("Which percentage to increase salary?");
+		double percentage = input.nextDouble();
+		
+		System.out.printf("Updated data: %s, $ %.2f", employee.name,employee.increaseSalary(percentage/100));
+					
+		input.close();
+	}
+
+}
